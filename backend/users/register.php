80780fc55f565bbc764139f35e38d1817ed9af15
charindu_edirisuriya <charinduedirisuriya@gmail.com>
2019-02-18 18:53:03 +0530

    Added Database Connectivity

--- backend/users/register.php
+++ backend/users/register.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: E.A.T.A Edirisuriya
- * Date: 2/18/2019
- * Time: 6:37 PM
- */
+
+$serverName = 'iitsdgp.database.windows.net';
+$uid = 'foodiecentral101';
+$pwd = 'foodiepassword69!';
+$connectionInfo = array( "UID"=>$uid,
+    "PWD"=>$pwd,
+    "Database"=>"FoodieCentral");
+
+    $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
