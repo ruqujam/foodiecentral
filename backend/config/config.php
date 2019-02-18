c26bd42858b2d68593fc023bc72947e9c939b4ec
VihangaK17 <vihangakarunasekara@gmail.com>
2019-02-18 19:06:59 +0530

    initialize config file

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: HP
- * Date: 2/18/2019
- * Time: 18:43
- */
+$serverName = 'iitsdgp.database.windows.net';
+$uid = 'foodiecentral101';
+$pwd = 'foodiepassword69!';
+$connectionInfo = array( "UID"=>$uid,
+    "PWD"=>$pwd,
+    "Database"=>"FoodieCentral");
+
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
--- backend/users/register.php
+++ backend/users/register.php
@@ -1,22 +1,2 @@
 <?php
 
-$serverName = 'iitsdgp.database.windows.net';
-$uid = 'foodiecentral101';
-$pwd = 'foodiepassword69!';
-$connectionInfo = array( "UID"=>$uid,
-    "PWD"=>$pwd,
-    "Database"=>"FoodieCentral");
-
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-
-function generateRandomString($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-
